3c0450f28a92477d4c8fef67eea483811ea61a18
James Joseph <31758423+chomes@users.noreply.github.com>
2018-03-27 17:56:02 +0100

    basic menu set up, need to now create questions.

---
+++ question_game_funcs.py
@@ -0,0 +1,200 @@
+
+# Function for ending the game
+def game_over():
+    print('''
+                                    You loose!
+                                    
+                               __xxxxxxxxxxxxxxxx___.
+                        _gxXXXXXXXXXXXXXXXXXXXXXXXX!x_
+                   __x!XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!x_
+                ,gXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx_
+              ,gXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!_
+            _!XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!.
+          gXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXs
+        ,!XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!.
+       g!XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+      iXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+     ,XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
+     !XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
+   ,XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
+   !XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXi
+  dXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+  !XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+   XXXXXXXXXXXXXXXXXXXf~~~VXXXXXXXXXXXXXXXXXXXXXXXXXXvvvvvvvvXXXXXXXXXXXXXX!
+   !XXXXXXXXXXXXXXXf`       'XXXXXXXXXXXXXXXXXXXXXf`          '~XXXXXXXXXXP
+    vXXXXXXXXXXXX!            !XXXXXXXXXXXXXXXXXX!              !XXXXXXXXX
+     XXXXXXXXXXv`              'VXXXXXXXXXXXXXXX                !XXXXXXXX!
+     !XXXXXXXXX.                 YXXXXXXXXXXXXX!                XXXXXXXXX
+      XXXXXXXXX!                 ,XXXXXXXXXXXXXX                VXXXXXXX!
+      'XXXXXXXX!                ,!XXXX ~~XXXXXXX               iXXXXXX~
+       'XXXXXXXX               ,XXXXXX   XXXXXXXX!             xXXXXXX!
+        !XXXXXXX!xxxxxxs______xXXXXXXX   'YXXXXXX!          ,xXXXXXXXX
+         YXXXXXXXXXXXXXXXXXXXXXXXXXXX`    VXXXXXXX!s. __gxx!XXXXXXXXXP
+          XXXXXXXXXXXXXXXXXXXXXXXXXX!      'XXXXXXXXXXXXXXXXXXXXXXXXX!
+          XXXXXXXXXXXXXXXXXXXXXXXXXP        'YXXXXXXXXXXXXXXXXXXXXXXX!
+          XXXXXXXXXXXXXXXXXXXXXXXX!     i    !XXXXXXXXXXXXXXXXXXXXXXXX
+          XXXXXXXXXXXXXXXXXXXXXXXX!     XX   !XXXXXXXXXXXXXXXXXXXXXXXX
+          XXXXXXXXXXXXXXXXXXXXXXXXx_   iXX_,_dXXXXXXXXXXXXXXXXXXXXXXXX
+          XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXP
+          XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+           ~vXvvvvXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXf
+                    'VXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXvvvvvv~
+                      'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX~
+                  _    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXv`
+                 -XX!  !XXXXXXX~XXXXXXXXXXXXXXXXXXXXXX~   Xxi
+                  YXX  '~ XXXXX XXXXXXXXXXXXXXXXXXXX`     iXX`
+                  !XX!    !XXX` XXXXXXXXXXXXXXXXXXXX      !XX
+                  !XXX    '~Vf  YXXXXXXXXXXXXXP YXXX     !XXX
+                  !XXX  ,_      !XXP YXXXfXXXX!  XXX     XXXV
+                  !XXX !XX           'XXP 'YXX!       ,.!XXX!
+                  !XXXi!XP  XX.                  ,_  !XXXXXX!
+                  iXXXx X!  XX! !Xx.  ,.     xs.,XXi !XXXXXXf
+                   XXXXXXXXXXXXXXXXX! _!XXx  dXXXXXXX.iXXXXXX
+                   VXXXXXXXXXXXXXXXXXXXXXXXxxXXXXXXXXXXXXXXX!
+                   YXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXV
+                    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
+                    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXf
+                       VXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXf
+                         VXXXXXXXXXXXXXXXXXXXXXXXXXXXXv`
+                          ~vXXXXXXXXXXXXXXXXXXXXXXXf`
+                              ~vXXXXXXXXXXXXXXXXv~
+                                 '~VvXXXXXXXV~~
+                                       ~~
+
+    ''')
+
+
+# For answering all questions
+def peasant_victory():
+    print("""
+    You did well you didn't win the final questions but you did a great job, until we meet again!
+             
+    /(|
+    (  :
+   __\  \  _____
+ (____)  `|
+(____)|   |
+ (____).__|
+  (___)__.|_____
+    """)
+
+
+def true_victory():
+    print("""
+    You pushed past the limits and you achieved victory, congratulations you've earned your victory!
+    
+    ´´´´´´´´´¶¶*..**..*
+´´´´´´´´´¶¶¶ .*.*.*.
+¶¶¶´´´´´¶¶´¶*..**.*.
+´¶¶¶¶¶¶¶¶´´¶*.*.**..**.
+´´´¶¶´´´´´´¶¶¶¶¶¶¶¶¶*.*.*.*.*.
+´´´´´¶¶´¶¶´´´´¶¶¶¶*.*.**.
+´´´´¶¶´´´´´¶¶¶.**.**.*.
+´´´¶¶´¶¶¶¶´´¶*.*.*.**.
+´´¶¶¶¶¶´´¶¶´¶´´´´´´´´¶*.**.*.*.*
+´¶¶´´´´´´´´¶¶¶´´´´´´¶¶*..**.*.
+´´´´´´´´´´´´¶¶´´´´´¶¶¶*.*.**.*
+´´´´´´´´´´´´´´´´´´¶¶¶¶¶*.*.**.*.*
+´´´´´´´´´´´´´´´´´¶¶¶¶¶¶*.*.*.*.*
+´´´¶¶¶¶¶´´´´´´´´¶¶¶´´¶¶ *.**.*.*
+´´´´´¶¶¶¶¶¶¶¶¶¶¶¶¶´´´¶¶*..**.**.*
+´´´´´´¶¶¶¶´´¶¶¶¶¶´´´´¶¶¶*.**.*.* /
+´´´´´´´´¶¶¶´´´´´´´´´´¶¶¶¶¶¶¶¶¶¶¶*.*.**…
+´´´´´´´´´¶¶¶¶´´´¶¶¶¶´´´´´´´´¶¶¶¶¶¶¶¶*….
+´´´´´´´´´´´¶¶¶¶´¶¶¶¶´´´´´¶¶¶¶¶¶*.**.*
+´´´´´´´´´´´¶¶¶´´´´´¶´¶¶¶¶¶¶*.*.*.**.
+´´´´´´´´´´¶¶´´´´´¶´´´´¶¶*.*.*.**.
+´´´´´´´´´¶¶´´¶¶¶¶¶¶´´´¶¶¶*.*.*.*
+´´´´´´´´¶¶¶¶¶¶¶¶´¶¶¶¶´¶¶¶ .**.*.
+´´´´´´´¶¶¶¶¶´´´´´´´¶¶¶¶¶¶ *.**.*
+´´´´´¶¶¶¶´´´´´´´´´´´¶¶¶¶¶ *.**.
+´´´´´¶´´´´´´´´´´´´´´´´¶¶¶*.*.*.
+´´´´´´´´´´´´´´´´´´´´´´´¶¶¶*.*.**.
+´´´´´´´´´´´´´´´´´´´´´´´´¶*.*.**.*
+
+             |                              ____.......__
+             |\      .'           _.--""''``             ``''--._
+             | \   .'/      ..--'`                             .-'`
+      .._    |  \.' /  ..-''                                .-'
+       '.`'"-:  '  .-'`                                  .-'
+         '.             __...----''''''''''--..           \
+         -         ..-''                       ``""-._     \
+       .'  _.      \                                  `"-   \
+      _.-'` |  /-.  \                                    `-. \
+            | /   `. \                                      `.\
+            |/      `-\                                       `.
+            |
+
+    
+    """)
+
+
+# Just a quick menu for displaying the full list of topics
+def full_topics_menu(full_list):
+    for topics in full_list:
+        print(topics)
+
+
+# Quick menu for displaying un-completed topics
+def partial_topics_menu(question_list):
+    for topics in question_list:
+        print(topics)
+
+
+# Running the question games
+def question_time(choose_topic):
+    if choose_topic == "movies":
+        movie_questions()
+    elif choose_topic == "music":
+        music_questions()
+    elif choose_topic == "games":
+        games_questions()
+    elif choose_topic == "comics"
+        comic_questions()
+
+
+# Game selection time
+def game_selection(completed_topics):
+    full_list = ["movies", "music", "games", "comics"]
+    if not completed_topics:
+        full_topics_menu(full_list)
+        while True:
+            choose_topic = input("Please choose from the list of topics:")
+            if choose_topic in full_list:
+                question_time(choose_topic)
+                break
+            else:
+                print("That topic is not in the listed, here is the list please try again")
+                full_topics_menu(full_list)
+                continue
+    elif full_list == completed_topics:
+        print("Congratulations on coming this far, you've won the game...but wait there's more!")
+        print("You will be taken to the secret bonus round of questions from different topics, get it right and you'll be the supreme winner")
+        print("Get it wrong..you've still won but it won't be as amazing, lets go!")
+        secret_questions()
+    else:
+        question_list = full_list[:]
+        for topics in completed_topics:
+            question_list.remove(topics)
+
+        partial_topics_menu(question_list)
+        while True:
+            choose_topic = input("Please choose from the list of topics:")
+            if choose_topic in question_list:
+                question_time(choose_topic)
+                break
+            else:
+                print("That topic is not in the listed, here is the list please try again")
+                partial_topics_menu(question_list)
+                continue
